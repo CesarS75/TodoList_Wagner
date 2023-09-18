92936f70b12c5efbc2b4ffc3249baf1a0e4383b4
CesarS75 <cesar.savoldi@etec.sp.gov.br>
2023-09-18 12:26:46 -0300

    seila

--- js/main.js
+++ js/main.js
@@ -1,1 +1,9 @@
-const test = document.querySelector('.container');
+// array para guardar a tarefa
+
+const tarefa = [
+    {'tarefa': 'Ler um livro bom'},
+    {'tarefa': 'Correr uma maratona'},
+];
+
+localStorage.setItem('tarefa', JSON.stringify(tarefa));
+JSON.parse(localStorage.getItem('tarefa'));
